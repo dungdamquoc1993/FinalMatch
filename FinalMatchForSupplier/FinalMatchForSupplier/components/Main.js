dc808724a7e6ef79a1a6577b36f25b18b607fede
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-11-17 16:03:12 +0700

    add button

--- FinalMatchForSupplier/FinalMatchForSupplier/components/Main.js
+++ FinalMatchForSupplier/FinalMatchForSupplier/components/Main.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {StyleSheet, View, Text} from 'react-native'
+import {StyleSheet, View, Text, Button} from 'react-native'
 export default class Main extends React.Component {
     state = {
         x: 0
@@ -9,6 +9,9 @@ export default class Main extends React.Component {
             <Text style = {styles.helloText}>
                 Chao cac ban
             </Text>
+            <Button onPress={() => {
+                alert("do somethingg")
+            }}>Lam gi do</Button>
         </View>
     }
 }
