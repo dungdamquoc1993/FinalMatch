8ce94811becc319543b25824924c13f60dda072f
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-11-28 17:28:46 +0700

    update

--- FinalMatchForSupplier/server/googleServices.js
+++ FinalMatchForSupplier/server/googleServices.js
@@ -1,20 +1,23 @@
 import {urlGetAddressFromLatLong} from './urlNames'
 export const getAddressFromLatLong = async (latitude, longitude) => {
     try {
+        debugger
         const response = await fetch(
             urlGetAddressFromLatLong(latitude, longitude)    
         );
+        debugger
         const responseJson = await response.json();
-        if (response.ok == true) {
-            //console.log(JSON.stringify(responseJson.results[0]["address_components"][0]["long_name"]))
-            if (responseJson.results.length > 0) {
-                const address = responseJson.results[0]["address_components"][0]["long_name"]
-                const district = responseJson.results[0]["address_components"][1]["long_name"]
-                const province = responseJson.results[0]["address_components"][2]["long_name"]
-                return { address, district, province }
-            }
-            return { address: '', district: '', province:''}
-        }
+        debugger
+        // if (response.ok == true) {
+        //     //console.log(JSON.stringify(responseJson.results[0]["address_components"][0]["long_name"]))
+        //     if (responseJson.results.length > 0) {
+        //         const address = responseJson.results[0]["address_components"][0]["long_name"]
+        //         const district = responseJson.results[0]["address_components"][1]["long_name"]
+        //         const province = responseJson.results[0]["address_components"][2]["long_name"]
+        //         return { address, district, province }
+        //     }
+        //     return { address: '', district: '', province:''}
+        // }
     } catch (error) {
         console.error(`Cannot get Address From Lat Long. Error: ${error}`)
         return { address: '', district: '', province:''}
