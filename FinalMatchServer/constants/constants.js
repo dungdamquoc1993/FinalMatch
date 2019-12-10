cdc163ce6134f02abe103ff2b2c9a9d1e9b1b87b
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-12-10 17:30:58 +0700

    update

--- FinalMatchServer/constants/constants.js
+++ FinalMatchServer/constants/constants.js
@@ -1,6 +1,7 @@
 const HOSTNAME = '127.0.0.1'
 const PORT = 3000
-
+const DB_PORT = 3306
+const DB_NAME = "FinalMatch"
 module.exports = {
-    HOSTNAME, PORT
+    HOSTNAME, PORT, DB_PORT, DB_NAME
 }
--- FinalMatchServer/routings/SupplierRoute.js
+++ FinalMatchServer/routings/SupplierRoute.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var router = express.Router()
-
+const {connection} = require('../database/database')
 // define the home page route
 router.get('/', async (req, res) => {
   //....
@@ -11,7 +11,20 @@ router.get('/', async (req, res) => {
 router.post('/register', async (req, res) => {
   //....
   const {email, password, userType='default'} = req
-  
+  debugger
+  connection.query('SELECT * FROM Supplier', (err, result, fields) => {
+    debugger;
+    console.log("haha")
+  })
+
+  /**
+   * 
+   * connection.query('SELECT ?? FROM ?? WHERE id = ?', [columns, 'users', userId], function (error, results, fields) {
+  if (error) throw error;
+  // ...
+});
+
+   */
   res.json({name: "Hoang", age: 30, time: Date.now()})
 })
 module.exports = router
