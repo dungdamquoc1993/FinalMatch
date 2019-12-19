ac0e39ed8de7093f0c67fc2a26d3c6474c473c16
Nguyen Duc Hoang <sunlight4d@gmail.com>
2019-12-20 00:23:51 +0700

    update

--- FinalMatchServer/index.js
+++ FinalMatchServer/index.js
@@ -1,5 +1,5 @@
 /**
- * yarn add express http 
+ * yarn add express http request express mysql
  */
 const { HOSTNAME, PORT} = require('./constants/constants')
 const {app} = require('./server')
@@ -11,6 +11,7 @@ app.use(bodyParser.json())
 const SupplierRoute = require('./routings/SupplierRoute')
 const PlayerServiceRoute = require('./routings/PlayerServiceRoute')
 const RefereeServiceRoute = require('./routings/RefereeServiceRoute')
+const GoogleServiceRoute = require('./routings/GoogleServiceRoute')
 const TokenRoute = require('./routings/TokenRoute')
 
 
@@ -18,6 +19,8 @@ app.use('/suppliers', SupplierRoute)
 app.use('/playerServices', PlayerServiceRoute)
 app.use('/refereeServices', RefereeServiceRoute)
 app.use('/token', TokenRoute)
+app.use('/googleServiceRoute', GoogleServiceRoute)
+
 
 app.listen(PORT, () => {
     console.log(`app listen from : ${PORT}`)
---
+++ FinalMatchServer/routings/GoogleServiceRoute.js
@@ -0,0 +1,38 @@
+var express = require('express')
+const https = require('https')
+var router = express.Router()
+const GoogleAPIKey = 'AIzaSyBrpg01q7yGyZK7acZuTRUw-HIrtFT-Zu0'
+const urlGetAddressFromLatLong = (latitude, longitude) => {
+    debugger
+    return `https://maps.googleapis.com/maps/api/geocode/json?address=${latitude},${longitude}&key=${GoogleAPIKey}`
+}
+router.get('/getAddressFromLatLong', async (req, res) => {    
+    const { latitude = '', longitude = '' } = req.query
+    https.get(urlGetAddressFromLatLong(latitude, longitude), (resp) => {
+        let data = '';
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            debugger
+            res.json({
+                result: "ok",
+                data: JSON.parse(data),
+                message: "Get lat/long successfully",
+                time: Date.now()
+            })    
+        });
+
+    }).on("error", (error) => {
+        res.json({
+            result: "failed",
+            data: {},
+            message: `Cannot get address from lat, long: ${error}`,
+            time: Date.now()
+        })
+    });    
+})
+module.exports = router
