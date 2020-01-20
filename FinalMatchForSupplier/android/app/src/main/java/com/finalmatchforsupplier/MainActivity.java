f33b1a85ba09eadb8170d018eaf8d142c5dccfaa
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-01-20 18:06:19 +0700

    update

--- FinalMatchForSupplier/android/app/src/main/java/com/finalmatchforsupplier/MainActivity.java
+++ FinalMatchForSupplier/android/app/src/main/java/com/finalmatchforsupplier/MainActivity.java
@@ -29,7 +29,6 @@ public class MainActivity extends ReactActivity {
               MessageDigest md = MessageDigest.getInstance("SHA");
               md.update(signature.toByteArray());
               Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-//              Log.d("KeyHash11:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
           }
       } catch (PackageManager.NameNotFoundException e) {
 
