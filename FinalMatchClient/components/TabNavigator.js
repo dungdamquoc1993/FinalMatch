92c7702fb46834ebf7ade8fee327db8af1743799
ciencute <ciencute1998@gmail.com>
2020-02-26 16:38:29 +0700

    code tab

--- FinalMatchClient/components/TabNavigator.js
+++ FinalMatchClient/components/TabNavigator.js
@@ -1,18 +1,35 @@
+import React from 'react'
 import { createBottomTabNavigator } from 'react-navigation-tabs'
 import Orders from './Orders'
 import Service from './Service'
 import { createAppContainer } from 'react-navigation'
+import {Image} from 'react-native'
 const Tabnavigator = createBottomTabNavigator(
     {
         Service:{screen: Service},
         Orders: { screen: Orders }
     },
     {
-        tabBarOptions: {
-            activeTintColor: 'tomato',
-            inactiveTintColor: 'gray',
-          },
+        defaultNavigationOptions: ({ navigation }) => ({
+            tabBarIcon: ({tintColor }) => {
+                const { routeName } = navigation.state;
+                let iconName = ""
+                switch (routeName) {
+                    case 'Service':
+                        iconName = require('../images/service.png')
+                        break
+                    case 'Orders':
+                        iconName = require('../images/deal.png')
+                        break
+                    default:
+                        iconName = ""
+                        break
+                }
+                return <Image source={iconName} style={{width:30,height:30}} />
+            },
+        }),
     }
+    
 )
 const MyTabnavigator = createAppContainer(Tabnavigator)
 MyTabnavigator.navigationOptions = {
