d98fbe1093fa0258095eaab97d2f0210ffef9c20
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-03-13 15:16:03 +0700

    update

--- FinalMatchServer/notifications/firebaseCloudMessaging.js
+++ FinalMatchServer/notifications/firebaseCloudMessaging.js
@@ -6,25 +6,25 @@ admin.initializeApp({
   databaseURL: "https://finalmatch-9f4fe.firebaseio.com"
 });
 
-const sendFirebaseCloudMessage = async ({title, body, payload, registrationTokens}) => {      
+const sendFirebaseCloudMessage = async ({title, body, payload, notificationTokens}) => {      
   try {
     debugger
-    if(registrationTokens.length == 0) {return}
+    if(notificationTokens.length == 0) {return}
     debugger
     let response = await admin.messaging().sendMulticast({
       data: {title, body, payload},
-      tokens: registrationTokens,
+      tokens: notificationTokens,
     })
     debugger
     if (response.failureCount > 0) {
       debugger
-      if(response.successCount == registrationTokens.length) {
+      if(response.successCount == notificationTokens.length) {
         console.log("send all notifications successfully")
       }
       const failedTokens = [];
       response.responses.forEach((resp, idx) => {
         if (!resp.success) {
-          failedTokens.push(registrationTokens[idx]);
+          failedTokens.push(notificationTokens[idx]);
         }
       });
       console.log('List of tokens that caused failures: ' + failedTokens);
