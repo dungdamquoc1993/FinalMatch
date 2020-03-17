7f43e867a7164f8d396ab5006307df11b88957ec
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-03-17 11:13:14 +0700

    update

--- FinalMatchServer/notifications/firebaseCloudMessaging.js
+++ FinalMatchServer/notifications/firebaseCloudMessaging.js
@@ -8,12 +8,15 @@ admin.initializeApp({
 
 const sendFirebaseCloudMessage = async ({title, body, payload, notificationTokens}) => {      
   try {
+    debugger
     const failedTokens = [];    
+    debugger
     if(notificationTokens.length == 0) {return}    
     let response = await admin.messaging().sendMulticast({
       data: {title, body},
       tokens: notificationTokens,
     })    
+    debugger
     if (response.failureCount > 0) {      
       if(response.successCount == notificationTokens.length) {
         console.log("send all notifications successfully")
@@ -25,9 +28,11 @@ const sendFirebaseCloudMessage = async ({title, body, payload, notificationToken
       })
       console.log('List of tokens that caused failures: ' + failedTokens);
     } 
+    debugger
     return failedTokens   
   }catch(error) {    
     console.log('Cannot send FCM.Error ='+error);
+    debugger
     return failedTokens
   }  
 }
