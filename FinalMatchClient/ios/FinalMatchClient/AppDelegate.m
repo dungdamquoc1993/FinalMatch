df122fd80c8de5bea4ceb400273d4157841bfbcb
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-03-17 12:11:00 +0700

    update

--- FinalMatchClient/ios/FinalMatchClient/AppDelegate.m
+++ FinalMatchClient/ios/FinalMatchClient/AppDelegate.m
@@ -158,5 +158,38 @@ NSString *const kGCMMessageIDKey = @"gcm.message_id";
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
 }
+- (void)pushLocalNotification:(NSString *)title message:(NSString *)message {
+  UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+  UNAuthorizationOptions options = UNAuthorizationOptionAlert + UNAuthorizationOptionSound+ UNAuthorizationOptionBadge;
+  [center requestAuthorizationWithOptions: options
+                        completionHandler:^(BOOL granted, NSError * _Nullable error) {
+//    NSLog(@"granted");
+//    NSLog(granted);
+  }];
+  UNMutableNotificationContent *content = [UNMutableNotificationContent new];
+  content.title = title;
+  content.body = message;
+  content.sound = [UNNotificationSound defaultSound];
+  
+  NSDate *date = [[NSDate new] dateByAddingTimeInterval:2];
+  
+  NSDateComponents *triggerDate = [[NSCalendar currentCalendar]
+                                   components:NSCalendarUnitYear +
+                                   NSCalendarUnitMonth + NSCalendarUnitDay +
+                                   NSCalendarUnitHour + NSCalendarUnitMinute +
+                                   NSCalendarUnitSecond fromDate:date];
+  
+  UNCalendarNotificationTrigger *trigger = [UNCalendarNotificationTrigger
+                                            triggerWithDateMatchingComponents:triggerDate
+                                            repeats:NO];
+  NSString *uuidString = [[NSUUID new] UUIDString];
+  UNNotificationRequest *request = [UNNotificationRequest requestWithIdentifier:uuidString
+                                                                        content:content trigger:trigger];
 
+  [center addNotificationRequest:request withCompletionHandler:^(NSError * _Nullable error) {
+    if (error != nil) {
+      NSLog(@"Something went wrong: %@",error);
+    }
+  }];
+}
 @end
