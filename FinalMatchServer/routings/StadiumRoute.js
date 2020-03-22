cec4331b1ca1393ba46b58dbdbfd8ccf3a9acd5c
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-03-22 20:50:32 +0700

    update

--- FinalMatchServer/routings/StadiumRoute.js
+++ FinalMatchServer/routings/StadiumRoute.js
@@ -52,7 +52,7 @@ router.post('/insertStadium', async (req, res) => {
                   res.json({
                     result: "ok", 
                     data: {stadiumName, supplierId }, 
-                    message: 'insert Stadium  successfully',
+                    message: "Insert Stadium  successfully",
                     time: Date.now()})
               }                
           }
@@ -103,7 +103,7 @@ router.post('/getStadiumsAroundPoint', async (req, res) => {
                       result: "ok",
                       count: results == null ? 0: results.length,
                       data: results[0],
-                      message: 'Query Stadiums successfully',
+                      message: "Query Stadiums successfully",
                       time: Date.now()})
                 }                
             }
