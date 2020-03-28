98980579da52e8c295a0de31f9ba536211e0eac4
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-03-28 16:50:44 +0700

    update

--- FinalMatchClient/server/myServices.js
+++ FinalMatchClient/server/myServices.js
@@ -314,6 +314,7 @@ export const getOrdersByCustomerId = async () => {
                 customerId,
             }),
         })    
+        debugger
                     
         const responseJson = await response.json()        
         
