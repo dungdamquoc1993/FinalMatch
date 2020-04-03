4ef52eb08cfc9e6d825958b56b6fddad159de6c6
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-04-03 16:27:33 +0700

    Stable date 03-04-2020

--- FinalMatchForSupplier/server/urlNames.js
+++ FinalMatchForSupplier/server/urlNames.js
@@ -4,7 +4,7 @@ const GoogleAPIKey = 'AIzaSyBrpg01q7yGyZK7acZuTRUw-HIrtFT-Zu0'
 //const SERVER_NAME = '192.168.1.149' //Giap Nhat
 // const SERVER_NAME = '192.168.1.88' //mrHoang
 const SERVER_NAME = '150.95.113.87' //Real Server
-const SERVER_PORT = '3001'
+const SERVER_PORT = '3000'
 
 export const urlRegisterSupplier = () => {
     return `http://${SERVER_NAME}:${SERVER_PORT}/suppliers/register`
