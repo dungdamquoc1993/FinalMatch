8c7c1434dc8651e6182ea6ed27c55a19fc8e148f
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-04-18 19:48:02 +0700

    update

--- AutomationTest/Helper.js
+++ AutomationTest/Helper.js
@@ -10,14 +10,19 @@ async function sendPost(url, paramObject) {
             },
             data: JSON.stringify(paramObject),
             url,
-        }        
+        }   
+        debugger     
         const response = await axios(options)        
         const responseObject = response.data
         const { result, data, message, time } = responseObject                
-        if(result.toUpperCase() == 'ok') {
+        debugger
+        if(result.toUpperCase() == 'OK') {
             print(url, '')
+        } else {
+            print(url, message)
         }
-        return true
+
+        return result.toUpperCase() == 'OK'
     } catch (error) {
         debugger
         print(url, error)
@@ -31,10 +36,12 @@ async function sendGet(url, paramObject) {
         const response = await axios.get(url, {params: paramObject})        
         const responseObject = response.data
         const { result, data, message, time } = responseObject                
-        if(result.toUpperCase() == 'ok') {
+        if(result.toUpperCase() == 'OK') {
             print(url, '')
+        } else {
+            print(url, message)
         }
-        return true
+        return result.toUpperCase() == 'OK'
     } catch (error) {
         debugger
         print(url, error)
@@ -44,8 +51,9 @@ async function sendGet(url, paramObject) {
 }
 
 function print(name, errorString) {
+    debugger
     if(errorString.length > 0) {
-        console.log('\x1b[31m%s\x1b[0m',`test FAILED : ${name}:\n ${errorString}`)
+        console.log('\x1b[31m%s\x1b[0m',`test FAILED : ${name}. Detail error: ${errorString}`)
     } else {
         console.log('\x1b[36m%s\x1b[0m',`test PASSED : ${name}`)
     }
