dbc843c59f8aa2c28cbe4cb94718a011cfe6ef1f
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-04-20 09:34:45 +0700

    update

--- FinalMatchServer/index.js
+++ FinalMatchServer/index.js
@@ -36,7 +36,7 @@ app.use('/chat', ChatRoute)
 app.use('/temp', TempRoute)
 app.use('/notifications', NotificationRoute)
 
-
+debugger
 PORT = require('readline-sync').question("Enter PORT : ");
 app.listen(PORT, () => {
     console.log(`app listen from : ${PORT}`)
