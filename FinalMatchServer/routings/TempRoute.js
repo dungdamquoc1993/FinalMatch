1cde6f1bed0c69a1de70e1dc5d2e10c0d47e6f7a
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-05-21 16:34:49 +0700

    update

--- FinalMatchServer/routings/TempRoute.js
+++ FinalMatchServer/routings/TempRoute.js
@@ -7,7 +7,9 @@ const POST_INSERT_HASHKEY = "INSERT INTO Temp(content, createdDate) VALUES(?, ?)
 
 //Link http://localhost:3000/temp/insertHashKey
 router.post('/insertHashKey', async (req, res) => {       
+    debugger
   const {content, locale} = req.body      
+  debugger
   i18n.setLocale(locale)
     connection.query(POST_INSERT_HASHKEY, [content, new Date()], (error, results) => {
         if (error) {
