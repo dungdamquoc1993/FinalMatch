f679598a6c6bf9ccb86356f9b87c48faf7fababf
Dam Quoc Dung <dungdamquoc1993@gmail.com>
2020-06-21 17:27:52 +0700

    update

--- FinalMatchClient/android/app/src/main/java/com/finalmatchclient/notifications/MyFirebaseMessagingService.java
+++ FinalMatchClient/android/app/src/main/java/com/finalmatchclient/notifications/MyFirebaseMessagingService.java
@@ -21,11 +21,34 @@ import androidx.core.app.NotificationManagerCompat;
 import java.util.HashMap;
 import java.util.Map;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.Map;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.os.Build;
+import android.os.Bundle;
+import androidx.core.app.NotificationCompat;
+import android.util.Log;
+
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "MessageService";
     private NotificationObject notificationObject = new NotificationObject();
+    public static final String FCM_PARAM = "picture";
+    private static final String CHANNEL_NAME = "FCM";
+    private static final String CHANNEL_DESC = "Firebase Cloud Messaging";
+    private int numMessages = 0;
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
         Log.d(TAG, "From: " + remoteMessage.getFrom());
         if (remoteMessage.getData().size() > 0) {
@@ -35,13 +58,10 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                     data.get("body") == null ? "" : data.get("body"),
                     new HashMap<>()
             );
-            //kiem tra va yeu cau cho phep noti
-            if(NotificationManagerCompat.from(this).areNotificationsEnabled() == false) {
-                System.out.println("haha");
-            } else {
-                System.out.println("nhuc");
-            }
-            this.pushRemoteNotification(notificationObject);
+            RemoteMessage.Notification notification = remoteMessage.getNotification();
+            Log.d("FROM", remoteMessage.getFrom());
+            sendNotification(notification, data);
+            //this.pushRemoteNotification(notificationObject);
         }
 
     }
@@ -81,5 +101,65 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         NotificationManagerCompat notificationManager =  NotificationManagerCompat.from(context);
         Notification noti = builder.build();
         notificationManager.notify(2, noti);
+    }
+    private void sendNotification(RemoteMessage.Notification notification, Map<String, String> data) {
+        Bundle bundle = new Bundle();
+        bundle.putString(FCM_PARAM, data.get(FCM_PARAM));
+
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtras(bundle);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, getString(R.string.notification_channel_id))
+                .setContentTitle("nhuc")
+                .setContentText("bod yde")
+                .setAutoCancel(true)
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                //.setSound(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.win))
+                .setContentIntent(pendingIntent)
+                .setContentInfo("Hello")
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+//                .setColor(getColor(R.color.colorAccent))
+                .setLights(Color.RED, 1000, 300)
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setNumber(++numMessages)
+                .setSmallIcon(R.drawable.ic_notification);
+
+        try {
+            notificationBuilder.setStyle(new NotificationCompat.BigTextStyle()
+                    .bigText("a lodishjuierw"));
+            String picture = data.get(FCM_PARAM);
+            if (picture != null && !"".equals(picture)) {
+                URL url = new URL(picture);
+                Bitmap bigPicture = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                notificationBuilder.setStyle(
+                        new NotificationCompat.BigPictureStyle().bigPicture(bigPicture).setSummaryText("nhuc chuhcuc")
+                );
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    getString(R.string.notification_channel_id), CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT
+            );
+            channel.setDescription(CHANNEL_DESC);
+            channel.setShowBadge(true);
+            channel.canShowBadge();
+            channel.enableLights(true);
+            channel.setLightColor(Color.RED);
+            channel.enableVibration(true);
+            channel.setVibrationPattern(new long[]{100, 200, 300, 400, 500});
+
+            assert notificationManager != null;
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        assert notificationManager != null;
+        notificationManager.notify(0, notificationBuilder.build());
     }
 }
