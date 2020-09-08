0372342dedf9288ae31135e2ab123b1760caad57
Vũ Văn Hoàn <hoanvv@arrowhitech.com>
2020-09-08 08:45:30 +0700

    add popup request notification permission

--- FinalMatchClient/components/Service.js
+++ FinalMatchClient/components/Service.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import {
   Text,
   View,
@@ -6,28 +6,68 @@ import {
   StyleSheet,
   Platform,
   TouchableOpacity,
+  Modal,
+  Linking,
+  AppState
 } from 'react-native';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import LinearGradient from 'react-native-linear-gradient'
-import {translate} from '../languages/languageConfigurations'
+import { translate } from '../languages/languageConfigurations'
 import MultiLanguageComponent from './MultiLanguageComponent'
 import Header from './Header'
+import messaging from '@react-native-firebase/messaging';
+
 export default class Service extends MultiLanguageComponent {
-  constructor (props) {
-    super (props);
-    this.state = {};
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+      appState: AppState.currentState
+    };
   }
-  
-  render () {
-    const {navigate} = this.props.navigation;
+
+  _handleAppStateChange = nextAppState => {
+    if (
+      this.state.appState.match(/inactive|background/) &&
+      nextAppState === "active"
+    ) {
+      this.checkNotifyPermission()
+    }
+    this.setState({ appState: nextAppState });
+  };
+
+  checkNotifyPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      this.setState({ visible: false })
+    } else {
+      this.setState({ visible: true })
+    }
+  }
+
+  async componentDidMount() {
+    AppState.addEventListener("change", this._handleAppStateChange);
+    this.checkNotifyPermission()
+  }
+
+  componentWillUnmount() {
+    AppState.removeEventListener("change", this._handleAppStateChange);
+  }
+
+  render() {
+    const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
-      
-          <View style = {{ flexDirection: 'column', alignItems: 'center', marginTop: 20}} >
-          <Text style={{fontSize: 20,margin: 10,textAlign: 'center', fontStyle: 'italic'}}>
-           {translate('Greater than just a Game')}</Text> 
-          <Text style={styles.textTitle}>{translate('Order service')}</Text> 
-          </View>
+
+        <View style={{ flexDirection: 'column', alignItems: 'center', marginTop: 20 }} >
+          <Text style={{ fontSize: 20, margin: 10, textAlign: 'center', fontStyle: 'italic' }}>
+            {translate('Greater than just a Game')}</Text>
+          <Text style={styles.textTitle}>{translate('Order service')}</Text>
+        </View>
 
         <LinearGradient
           colors={['#CAF1C1', '#C2F3B7', '#33FF99']}
@@ -39,7 +79,7 @@ export default class Service extends MultiLanguageComponent {
         >
           <TouchableOpacity
             onPress={() => {
-              navigate ('OrderPlayer');
+              navigate('OrderPlayer');
             }}
           >
             <View
@@ -50,11 +90,11 @@ export default class Service extends MultiLanguageComponent {
               }}
             >
               <Text style={styles.textButton}>
-              {translate('Order Player')}
+                {translate('Order Player')}
               </Text>
               <Image
-                style={{height: 30, width: 30}}
-                source={require ('../images/2339949.png')}
+                style={{ height: 30, width: 30 }}
+                source={require('../images/2339949.png')}
               />
             </View>
             <View
@@ -64,19 +104,19 @@ export default class Service extends MultiLanguageComponent {
                 justifyContent: 'space-around',
               }}
             >
-              <View style={{flexDirection: 'column'}}>
+              <View style={{ flexDirection: 'column' }}>
                 <Text>CF</Text>
                 <FontAwesome5 name={'check-square'} size={20} color={'black'} />
               </View>
-              <View style={{flexDirection: 'column'}}>
+              <View style={{ flexDirection: 'column' }}>
                 <Text>CF</Text>
                 <FontAwesome5 name={'check-square'} size={20} color={'black'} />
               </View>
-              <View style={{flexDirection: 'column'}}>
+              <View style={{ flexDirection: 'column' }}>
                 <Text>CF</Text>
                 <FontAwesome5 name={'check-square'} size={20} color={'black'} />
               </View>
-              <View style={{flexDirection: 'column'}}>
+              <View style={{ flexDirection: 'column' }}>
                 <Text>CF</Text>
                 <FontAwesome5 name={'check-square'} size={20} color={'black'} />
               </View>
@@ -94,7 +134,7 @@ export default class Service extends MultiLanguageComponent {
         >
           <TouchableOpacity
             onPress={() => {
-              navigate ("OrderReferee");
+              navigate("OrderReferee");
             }}
           >
             <View
@@ -106,11 +146,11 @@ export default class Service extends MultiLanguageComponent {
               }}
             >
               <Text style={styles.textButton}>
-              {translate("Order Referee")}
+                {translate("Order Referee")}
               </Text>
               <Image
-                style={{height: 30, width: 30}}
-                source={require ('../images/whistle.png')}
+                style={{ height: 30, width: 30 }}
+                source={require('../images/whistle.png')}
               />
             </View>
           </TouchableOpacity>
@@ -126,7 +166,7 @@ export default class Service extends MultiLanguageComponent {
         >
           <TouchableOpacity
             onPress={() => {
-              navigate ('Stadium');
+              navigate('Stadium');
             }}
           >
             <View
@@ -138,20 +178,47 @@ export default class Service extends MultiLanguageComponent {
               }}
             >
               <Text style={styles.textButton}>
-              {translate('Search Stadium')}
+                {translate('Search Stadium')}
               </Text>
               <Image
-                style={{height: 30, width: 30}}
-                source={require ('../images/404650.png')}
+                style={{ height: 30, width: 30 }}
+                source={require('../images/404650.png')}
               />
             </View>
           </TouchableOpacity>
         </LinearGradient>
+        <Modal
+          transparent={true}
+          visible={this.state.visible}
+          animated
+        >
+          <View style={styles.modal}>
+            <View style={{ backgroundColor: '#fff', borderRadius: 8 }}>
+              <View style={{ padding: 20 }}>
+                <Text style={{ fontWeight: 'bold' }}>Let FinalMatch Notify You</Text>
+              </View>
+              <TouchableOpacity style={styles.button} onPress={() => this.setState({ visible: false })}>
+                <Text style={{ textAlign: 'center', color: '#3079ff', fontWeight: 'bold' }}>Don't Allow</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.button} onPress={() => Linking.openSettings()}>
+                <Text style={{ textAlign: 'center', color: '#3079ff', fontWeight: 'bold' }}>OK</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
       </View>
     );
   }
 }
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
+  modal: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#00000080' },
+  button: {
+    paddingVertical: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTopColor: '#ddd',
+    borderTopWidth: 1
+  },
   container: {
     flexDirection: 'column',
     alignItems: 'center',
@@ -165,7 +232,7 @@ const styles = StyleSheet.create ({
   },
   textButton: {
     fontSize: 20,
-    
+
     textAlign: 'center',
   },
 });
--- FinalMatchForSupplier/components/ServiceRegister.js
+++ FinalMatchForSupplier/components/ServiceRegister.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import {
   Text,
   View,
@@ -6,15 +6,18 @@ import {
   TouchableOpacity,
   SafeAreaView,
   Dimensions,
+  AppState,
+  Modal,
+  Linking
 } from 'react-native'
 import Header from './Header'
 
 import RefereeService from './RefereeService'
-import {translate} from '../languages/languageConfigurations'
+import { translate } from '../languages/languageConfigurations'
 import Stadium from './Stadium'
 import PlayerService from './PlayerService'
-import {connect} from 'react-redux'
-import {MAIN_COLOR, COLOR_BUTTON} from '../colors/colors'
+import { connect } from 'react-redux'
+import { MAIN_COLOR, COLOR_BUTTON } from '../colors/colors'
 import {
   checkPlayerServiceExist,
   checkRefereeServiceExist,
@@ -24,25 +27,66 @@ import {
   saveSupplierToStorage,
   isIOS,
 } from '../helpers/Helpers'
-const screenWidth = Math.round (Dimensions.get ('window').width)
-const screenHeight = Math.round (Dimensions.get ('window').height)
+import messaging from '@react-native-firebase/messaging';
+
+const screenWidth = Math.round(Dimensions.get('window').width)
+const screenHeight = Math.round(Dimensions.get('window').height)
 
 class ServiceRegister extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+      appState: AppState.currentState
+    }
+  }
+  _handleAppStateChange = nextAppState => {
+    if (
+      this.state.appState.match(/inactive|background/) &&
+      nextAppState === "active"
+    ) {
+      this.checkNotifyPermission()
+    }
+    this.setState({ appState: nextAppState });
+  };
+
+  checkNotifyPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      this.setState({ visible: false })
+    } else {
+      this.setState({ visible: true })
+    }
+  }
+
+  async componentDidMount() {
+    AppState.addEventListener("change", this._handleAppStateChange);
+    this.checkNotifyPermission()
+  }
+
+  componentWillUnmount() {
+    AppState.removeEventListener("change", this._handleAppStateChange);
+  }
+
   _navigateToPlayerService = async () => {
-    const {supplierId, tokenKey, email} = await getSupplierFromStorage ()
-    const {data, message} = await checkPlayerServiceExist (supplierId)
-    const {numberOfPlayerServices} = data
-    if (parseInt (numberOfPlayerServices) == 0) {
+    const { supplierId, tokenKey, email } = await getSupplierFromStorage()
+    const { data, message } = await checkPlayerServiceExist(supplierId)
+    const { numberOfPlayerServices } = data
+    if (parseInt(numberOfPlayerServices) == 0) {
       this.props.stackNavigation.navigate('PlayerService', {})
     } else {
       this.props.navigation.navigate(translate("Settings"), {})
     }
   }
   _navigateToRefereeService = async () => {
-    const {supplierId, tokenKey, email} = await getSupplierFromStorage ()
-    const {data, message} = await checkRefereeServiceExist (supplierId)
-    const {numberOfRefereeServices} = data
-    if (parseInt (numberOfRefereeServices) == 0) {
+    const { supplierId, tokenKey, email } = await getSupplierFromStorage()
+    const { data, message } = await checkRefereeServiceExist(supplierId)
+    const { numberOfRefereeServices } = data
+    if (parseInt(numberOfRefereeServices) == 0) {
       this.props.stackNavigation.navigate('RefereeService', {})
     } else {
       this.props.navigation.navigate(translate("Settings"), {})
@@ -51,21 +95,21 @@ class ServiceRegister extends Component {
   _navigateToStadium = () => {
     this.props.stackNavigation.navigate('Stadium', {})
   }
-  render () {
+  render() {
     return (
       <SafeAreaView style={{
-          flex: 1,          
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}>        
-        <Header           
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+        <Header
           title={translate("Register a service")} hideBack={true} hideNext={true} />
-        <View style={{alignItems: 'center', justifyContent: 'center'}}>
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
           <TouchableOpacity
             style={styles.button}
             onPress={() => {
-              this._navigateToPlayerService ()
+              this._navigateToPlayerService()
             }}
           >
             <Text style={styles.txt}>
@@ -75,17 +119,17 @@ class ServiceRegister extends Component {
           <TouchableOpacity
             style={styles.button}
             onPress={() => {
-              this._navigateToRefereeService ()
+              this._navigateToRefereeService()
             }}
           >
             <Text style={styles.txt}>
-            {translate("Referee")}
+              {translate("Referee")}
             </Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={styles.button}
             onPress={() => {
-              this._navigateToStadium ()
+              this._navigateToStadium()
             }}
           >
             <Text style={styles.txt}>
@@ -93,7 +137,25 @@ class ServiceRegister extends Component {
             </Text>
           </TouchableOpacity>
         </View>
-
+        <Modal
+          transparent={true}
+          visible={this.state.visible}
+          animated
+        >
+          <View style={styles.modal}>
+            <View style={{ backgroundColor: '#fff', borderRadius: 8 }}>
+              <View style={{ padding: 20 }}>
+                <Text style={{ fontWeight: 'bold' }}>Let FinalMatch Notify You</Text>
+              </View>
+              <TouchableOpacity style={styles.buttonModal} onPress={() => this.setState({ visible: false })}>
+                <Text style={{ textAlign: 'center', color: '#3079ff', fontWeight: 'bold' }}>Don't Allow</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.buttonModal} onPress={() => Linking.openSettings()}>
+                <Text style={{ textAlign: 'center', color: '#3079ff', fontWeight: 'bold' }}>OK</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </Modal>
       </SafeAreaView>
     )
   }
@@ -103,12 +165,20 @@ const mapStateToProps = state => ({
   stackNavigation: state.navigationReducers.stackNavigation,
   tabNavigation: state.navigationReducers.tabNavigation,
 })
-export default connect (mapStateToProps) (ServiceRegister)
-const styles = StyleSheet.create ({
+export default connect(mapStateToProps)(ServiceRegister)
+const styles = StyleSheet.create({
+  modal: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#00000080' },
+  buttonModal: {
+    paddingVertical: 12,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderTopColor: '#ddd',
+    borderTopWidth: 1
+  },
   txt: {
-    lineHeight: isIOS() ? null: 0.08 * screenHeight,
-    fontSize: 22,    
-    color: 'white',    
+    lineHeight: isIOS() ? null : 0.08 * screenHeight,
+    fontSize: 22,
+    color: 'white',
   },
   button: {
     backgroundColor: MAIN_COLOR,
@@ -118,7 +188,7 @@ const styles = StyleSheet.create ({
     margin: 30,
     fontSize: 30,
     width: 0.8 * screenWidth,
-    height: 80,    
+    height: 80,
     borderRadius: 25,
     justifyContent: 'center',
     alignItems: 'center',
